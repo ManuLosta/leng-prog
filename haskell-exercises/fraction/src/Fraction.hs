1b2202a7bd16ba81e6233a7e053d90c8a3b688ce
ManuLosta <mlostalom@gmail.com>
2024-07-30 15:40:07 -0300

    done Fraction

--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -2,30 +2,53 @@ module Fraction (Fraction, add, sub, mul, divide, hcf) where
 
 type Fraction = (Int, Int)
 
+-- Helper methods
+
+num :: Fraction -> Int
+num (a, _) = a
+
+den :: Fraction -> Int
+den (_, a) = a
+
+simplify :: Fraction -> Fraction
+simplify (a, b) = (div a c, div b c)
+  where
+    c = hcf a b
+
 -- Implement the `add` Function
 
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
+add n d = simplify (numerator, denominator)
+  where
+    denominator = den n * den d
+    numerator = num n * den d + num d * den n
 
 -- Implement the `sub` Function
 
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub n d = simplify (numerator, denominator)
+  where
+    denominator = den n * den d
+    numerator = num n * den d - num d * den n
 
 -- Implement the `mul` Function
 
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul n d = simplify (numerator, denominator)
+  where
+    numerator = num n * num d
+    denominator = den n * den d
 
 -- Implement the `divide` Function
 
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide n d = simplify (numerator, denominator)
+  where
+    numerator = num n * den d
+    denominator = den n * num d
 
 -- Implement the `hcf` Function
 
 hcf :: Int -> Int -> Int
-hcf n d = error "Implement it"
-
-
-    
+hcf n 0 = n
+hcf n d = hcf d (mod n d)
--- haskell-exercises/fraction/test/Tests.hs
+++ haskell-exercises/fraction/test/Tests.hs
@@ -1,18 +1,17 @@
 -- {-# LANGUAGE RecordWildCards #-}
 
 --import Data.Foldable (for_)
-import Test.Hspec    (describe, hspec, it, shouldBe, Spec)
 
-import Fraction (Fraction, add, sub, mul, divide, hcf)
+import Fraction (Fraction, add, divide, hcf, mul, sub)
+import Test.Hspec (Spec, describe, hspec, it, shouldBe)
 
 main :: IO ()
-
 -- Comment this line and uncomment the line below to run the tests doing simplification
-main = hspec $ test id
--- main = hspec $ test simplify
+--main = hspec $ test id
+main = hspec $ test simplify
 
 test :: (Fraction -> Fraction) -> Spec
-test s = do 
+test s = do
   describe "add" $ do
     it "1/2 + 1/4" $ do
       (1, 2) `add` (1, 4) `shouldBe` s (6, 8)
@@ -24,16 +23,14 @@ test s = do
       (1, 2) `mul` (1, 4) `shouldBe` s (1, 8)
   describe "divide" $ do
     it "1/2 / 1/4" $ do
-       (1, 2) `divide` (1, 4) `shouldBe` s (4, 2)
-  describe "highest common factor"  $ do
+      (1, 2) `divide` (1, 4) `shouldBe` s (4, 2)
+  describe "highest common factor" $ do
     it "24 and 30" $ do
       hcf 24 30 `shouldBe` 6
     it "30 and 24" $ do
       hcf 30 24 `shouldBe` 6
-      
-simplify::Fraction -> Fraction
+
+simplify :: Fraction -> Fraction
 simplify (n, d) = (n `div` h, d `div` h)
-  where h = gcd n d           
-      
-      
-         
+  where
+    h = gcd n d
