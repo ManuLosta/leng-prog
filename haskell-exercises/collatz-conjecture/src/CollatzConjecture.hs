916eb7d00493ea743e7eb240b773f7d6fff21df6
ManuLosta <mlostalom@gmail.com>
2024-07-31 14:16:24 -0300

    done collatz

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,11 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n = collatz2 n 0 
+
+collatz2 :: Integer -> Integer -> Maybe Integer
+collatz2 n i 
+  | n <= 0 = Nothing
+  | n == 1 = Just i
+  | even n = collatz2 (n `div` 2) (i + 1)
+  | otherwise = collatz2 (3*n + 1) (i + 1)
