134e343e38f3c16f66ad1f6f7e043b28676f4ed4
ManuLosta <mlostalom@gmail.com>
2024-08-05 18:04:00 -0300

    implement union, intersection and difference

--- haskell-exercises/lists/src/Lists.hs
+++ haskell-exercises/lists/src/Lists.hs
@@ -1,30 +1,46 @@
-module Lists (member, union, intersection, difference,
-              insert, insertionSort,
-              binaryToDecimal, toDecimal, toDec, decimal,
-              binaryAdd) where
-  
-import Data.Char(digitToInt)  
+module Lists
+  ( member,
+    union,
+    intersection,
+    difference,
+    insert,
+    insertionSort,
+    binaryToDecimal,
+    toDecimal,
+    toDec,
+    decimal,
+    firsts,
+    binaryAdd,
+  )
+where
 
-member:: Int -> [Int] -> Bool
-member _ []      = False
-member e (x:xs)  = e == x || member e xs
+import Data.Char (digitToInt)
 
+member :: Int -> [Int] -> Bool
+member _ [] = False
+member e (x : xs) = e == x || member e xs
 
-union:: [Int] -> [Int] -> [Int]
-union [] ys     = ys
-union (x:xs) ys 
+union :: [Int] -> [Int] -> [Int]
+union [] ys = ys
+union (x : xs) ys
   | member x ys = union xs ys
-  | otherwise   = x : union xs ys
+  | otherwise = x : union xs ys
 
 -- Remove Implementations, from, here on
 
-intersection:: [Int] -> [Int] -> [Int]
-intersection = error "Implement it"
+intersection :: [Int] -> [Int] -> [Int]
+intersection [] _ = []
+intersection (x : xs) ys
+  | member x ys = x : intersection xs ys
+  | otherwise = intersection xs ys
 
-difference:: [Int] -> [Int] -> [Int]
-difference  = error "Implement it"
+difference :: [Int] -> [Int] -> [Int]
+difference [] _ = []
+difference (x : xs) ys
+  | member x ys = difference xs ys
+  | otherwise = x : difference xs ys
 
-insert:: Int -> [Int] -> [Int]
+insert :: Int -> [Int] -> [Int]
 insert = error "Implement it"
 
 insertionSort :: [Int] -> [Int]
@@ -32,23 +48,23 @@ insertionSort = error "Implement it"
 
 binaryToDecimal :: [Int] -> Int
 binaryToDecimal = error "Implement it"
-    
+
 toDecimal :: Int -> [Int] -> Int
 toDecimal = error "Implement it"
-    
-toDec::Int -> String -> Int
-toDec base s =  = error "Implement it"
+
+toDec :: Int -> String -> Int
+toDec base s = error "Implement it"
 
 -- Same as `toDec` But use a list comprehension
 
-decimal::Int -> String -> Int
-decimal  = error "Implement it"
+decimal :: Int -> String -> Int
+decimal = error "Implement it"
 
-firsts::[a] -> [[a]]
+firsts :: [a] -> [[a]]
 firsts = error "Implement it"
 
 -- Given two String that represents numbers in binary implement the 'binaryAdd' function
 -- DO NOT USE a predefined '+' operation
 
-binaryAdd::String -> String -> String
-binaryAdd  = error "Implement it"
+binaryAdd :: String -> String -> String
+binaryAdd = error "Implement it"
