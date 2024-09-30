4c0981b9fe31994f9e61bb6f92e65959e9e1e61b
ManuLosta <mlostalom@gmail.com>
2024-09-30 14:41:41 -0300

    collatz in rust

--- rust-exercises/collatz-conjecture/src/lib.rs
+++ rust-exercises/collatz-conjecture/src/lib.rs
@@ -1,4 +1,11 @@
 pub fn collatz(mut n: u64) -> Option<u64> {
-  None
+    if n == 0 {
+        None
+    } else if n == 1 {
+        Some(0)
+    } else if n % 2 == 0 {
+        Some(1 + collatz(n / 2).unwrap())
+    } else {
+        Some(1 + collatz(3 * n + 1).unwrap())
+    }
 }
-
