2ca994a73e2fbde21eb3bcb5e5dc1eff03d79abc
ManuLosta <mlostalom@gmail.com>
2024-09-30 14:21:13 -0300

    fraction in rust done

--- rust-exercises/fraction/src/lib.rs
+++ rust-exercises/fraction/src/lib.rs
@@ -2,30 +2,33 @@ type Fraction = (i32, i32);
 
 /// Add 2 fractions
 pub fn add((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    simplify(n1 * d2 + n2 * d1, d1 * d2)
 }
 
 /// Subtract 2 fractions
 pub fn sub((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    simplify(n1 * d2 - n2 * d1, d1 * d2)
 }
 
 /// Multiply 2 fractions
 pub fn mul((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    simplify(n1 * n2, d1 * d2)
 }
 
 /// Divide 2 fractions
 pub fn divide((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    simplify(n1 * d2, d1 * n2)
 }
 
 /// Calculate the Highest common factor between 2 numbers
 pub fn hcf(a: i32, b: i32) -> i32 {
-    panic!("Implement it");
+    match b {
+        0 => a,
+        _ => hcf(b, a % b),
+    }
 }
 
 /// Create a fraction simplifying with the arguments simplified by the `hcf`
 pub fn simplify(n: i32, d: i32) -> Fraction {
-     (n, d)
+    (n / hcf(n, d), hcf(n, d))
 }
